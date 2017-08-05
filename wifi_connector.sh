5a660a732e6d66346a8e2d5593a2fa41038a0ebc
xp3kkv <xp3kkv@gmail.com>
2017-08-05 23:14:38 +0000

    Interface Upgraded

--- wifi_connector.sh
+++ wifi_connector.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-SSID="SSID"
-PASSWD="wifi_password"
-WIFACE="wireless_interface"
+echo "Type your wireless interface name"
+read WIFACE
+echo "Type the wifi SSID (ex. Telecom-12*****)"
+read SSID
+echo "Type the wifi password"
+read PASSWD 
 
 wpa_passphrase "$SSID" "$PASSWD" > .wpa.conf
-wpa_supplicant -B -i $WIFACE -c .wpa.conf
-dhcpcd
-
+sudo wpa_supplicant -B -i $WIFACE -c .wpa.conf
+sudo dhcpcd
